67d062b526254a12881231854a5cca6d2678c5ea
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2018-08-17 15:00:20 -0300

    feat: Criação do componente Shelf

---
+++ src/Shelf.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
+import PropTypes from 'prop-types'
+
+class Shelf extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Shelf
