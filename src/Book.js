0b389c4d3c6e85a35f39badece7231f90284d87d
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2018-08-17 16:22:56 -0300

    feat: Atualização em Book

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Book extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired
+    }
     render() {
+        const { book } = this.props
+
+        const thumb = book.in('img')
+
+        if (thumb)
+            thumb = book.img.thumb
+        else
+            thumb = ''
+
         return (
-            <div></div>
+            <div className="book">
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{ width: 128, height: 193, backgroundImage: `url(${thumb})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">To Kill a Mockingbird</div>
+                <div className="book-authors">Harper Lee</div>
+            </div>
         )
     }
 }
