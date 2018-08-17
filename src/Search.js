a52ba57e45f2ff2fa73cc9a0e29909aa97833844
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2018-08-17 14:56:00 -0300

    feat: Adição de regras em Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import PropTypes from 'prop-types'
 
 class Search extends Component {
+    state = {
+        books: []
+    }
+    
+    static propTypes = {
+        
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({ books })
+        })
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim() })
+    }
+
+    clearQuery = () => {
+        this.setState({ query: '' })
+    }
+
     render() {
+        const { books, query } = this.state
+        let showingBooks
+
+        if (query) {
+            
+        } else {
+
+        }
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -11,6 +43,8 @@ class Search extends Component {
                         <input
                             type="text"
                             placeholder="Search by title or author"
+                            value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
                         />
                     </div>
                 </div>
