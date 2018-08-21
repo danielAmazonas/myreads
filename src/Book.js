19b4c617b202095fbdb65ef12254861e9b89ff97
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2018-08-21 16:33:13 -0300

    feat: Reformulação em Book

--- src/Book.js
+++ src/Book.js
@@ -4,77 +4,59 @@ import * as BooksAPI from './BooksAPI'
 
 class Book extends Component {
     static propTypes = {
-        shelf: PropTypes.array.isRequired,
+        book: PropTypes.object.isRequired,
+        onChanged: PropTypes.func.isRequired,
     }
 
-    state = {
-        books: []
-    }
-
-    componentDidMount() {
-        BooksAPI.getAll().then((books) => {
-            this.setState({ books })
-        })
+    changeShelf(e) {
+        this.props.onChanged(this.props.book, e.target.value)
     }
 
     render() {
-        const { books } = this.state
-        const { shelf } = this.props
+        const { book } = this.state
         let status
         const mark = '✓'
 
         return (
-            <ol className="books-grid">
-                {books.filter((book) => shelf.filter((s) => status = s.type).map((c) => c.id).includes(book.id)).map((book) => (
-                    <li key={book.id}>
-                        <div className="book">
-                            <div className="book-top">
-                                <div
-                                    className="book-cover"
-                                    style={{
-                                        width: 128,
-                                        height: 193,
-                                        backgroundImage: `url(${book.hasOwnProperty('imageLinks') ?
-                                            book.imageLinks.smallThumbnail :
-                                            ''})`
-                                    }}></div>
-                                <div className="book-shelf-changer">
-                                    <select value={status}>
-                                        <option
-                                            value="move"
-                                            disabled>Move to...</option>
-                                        <option
-                                            value="currentlyReading"
-                                            selected={status === 'currentlyReading' ? 'selected' : ''}>
-                                            {status === 'currentlyReading' ? mark : ''}
-                                            Currently Reading</option>
-                                        <option
-                                            value="wantToRead"
-                                            selected={status === 'wantToRead' ? 'selected' : ''}>
-                                            {status === 'wantToRead' ? mark : ''}
-                                            Want to Read</option>
-                                        <option
-                                            value="read"
-                                            selected={status === 'read' ? 'selected' : ''}>
-                                            {status === 'read' ? mark : ''}
-                                            Read</option>
-                                        <option
-                                            value="none">None</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="book-title">
-                                {book.title}
-                            </div>
-                            {book.authors ? book.authors.map((author) =>
-                                <div key={`${author}-${book.id}`} className="book-authors">
-                                    {author}
-                                </div>
-                            ) : ''}
-                        </div>
-                    </li>
-                ))}
-            </ol>
+
+            <div className="book">
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url(${book.hasOwnProperty('imageLinks') ?
+                                book.imageLinks.smallThumbnail :
+                                ''})`
+                        }}></div>
+                    <div className="book-shelf-changer">
+                        <select
+                            value={book.shelf}
+                            onChange={(event) => this.changeShelf(event)}>
+                            <option
+                                value="move"
+                                disabled>Move to...</option>
+                            <option
+                                value="currentlyReading">Currently Reading</option>
+                            <option
+                                value="wantToRead">Want to Read</option>
+                            <option
+                                value="read">Read</option>
+                            <option
+                                value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">
+                    {book.title}
+                </div>
+                {book.authors ? book.authors.map((author) =>
+                    <div key={`${author}-${book.id}`} className="book-authors">
+                        {author}
+                    </div>
+                ) : ''}
+            </div>
         )
     }
 }
